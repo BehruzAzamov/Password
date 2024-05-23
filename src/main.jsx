c69b5179ddf968420cac4561bdfb7f947e989f4d
azamovbehruz <azamovbehruz@gmail.com>
2024-05-23 22:04:46 +0500

    Ready

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PasswordGenerator from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className='container'>
+    <PasswordGenerator />
+  </div>,
+)
